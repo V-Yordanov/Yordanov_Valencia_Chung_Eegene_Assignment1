44b89347142913a3faff10e2070ceb7bf74ce952
Clara Chung <eegene.chung@cern.ch>
2019-05-13 12:46:56 -0400

    clara's edit

--- question1/question1.py
+++ question1/question1.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  4 22:28:23 2019
-
-@author: User
-"""
 import os
 import errno
 from math import *
 import numpy as np
 from scipy import *
-
-    
+import shutil
+ 
 def name_find_replace(name,find,replace):
     '''
     This function finds a file called 'name', and finds a word called find, and
@@ -19,40 +13,23 @@ def name_find_replace(name,find,replace):
     When calling this function make sure to add spaces before and after the 
     find and replace strings, so that the words are separated by a space
     '''
-
-    open_file = open(name, 'r')
-    contents = open_file.read()
     
-    newfile = open('replace.txt', 'w+')
-    contents = contents.replace(find,replace)
-    newfile.write(contents)
-    
-    
+    open_file = open(name, 'r') # open the file we want to replace
+    contents = open_file.read() # 'str': read the contents of this file
+    contents = contents.replace(find,replace) # replacae the word 'find' with 'replace'
 
-name_find_replace('testparagraph2.txt', ' Bob ', ' a cat ')
+    here = os.path.dirname(os.path.realpath(__file__)) # current directory
+    subdir = "replace" # new subdirectory, ie. our file name
+    filename = "replace.txt" # our new file name
+    filepath = os.path.join(here, subdir, filename) # create path to file
 
+    # create subdirectory
+    os.mkdir(os.path.join(here, subdir))
+    # create an file
+    f = open(filepath, 'w') # open the new file
+    f.write(contents) # write to the new file
+    f.close() # close the file
 
-
-def createDir(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print ('Error: Creating directory. ' +  directory)
         
-
-# Example
-createDir('./replace/')
-#
-#
-#
-#filename = "replace.txt"
-#os.makedirs(os.path.dirname(''), exist_ok=True)
-#with open(filename, "w+") as f:
-#    f.write("replace.txt")
-#
-#'c:/your/full/path'
-
-name_find_replacef = open(Yordanov_Valencia_Chung_Eegene_Assignment1/question1/replace, "a")
-
-    
+print("BEFORE YOU START, MAKE SURE THE FOLDER AND FILE 'replace' DOESN'T ALREADY EXIST IN YOUR DIRECTORY")
+name_find_replace('testparagraph2.txt', ' Bob ', ' a cat ')
